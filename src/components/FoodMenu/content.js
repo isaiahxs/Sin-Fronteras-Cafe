0c92e6c1abc2c11a8a2c22e29085aa513a41cf3b
isaiahxs <isaiahsinnathamby@gmail.com>
2023-09-03 21:46:48 -0400

    added drinks in FoodMenu content - need to move it to DrinksMenu

--- src/components/FoodMenu/content.js
+++ src/components/FoodMenu/content.js
@@ -278,5 +278,305 @@ export const menuEnglishData = {
             `,
             options: []
         },
+    ],
+    "White Wines": [
+        {
+            name: `Oyster Bay Rosé
+            `,
+            price: "Glass $7.50, Bottle $24.00",
+            description: `Oyster Bay's cool climate Marlborough Rosé exudes summer cherry and fragrant blossom notes with a burst of red berries and a lively citrus zest.
+            `,
+            options: []
+        },
+        {
+            name: `Tomaiolo Pinot Grigio, Italy
+            `,
+            price: "Glass $7.99, Bottle $24.99",
+            description: `Well balanced fruity flavors with a soft, dry finish.
+            `,
+            options: []
+        },
+        {
+            name: `Do Zoe Albarino, Spain
+            `,
+            price: "Glass $7.99, Bottle $24.99",
+            description: `Aromas of fruit, white flowers and mineral nuances which come from the vineyard. Round, well-balanced acidity maintaining a citrus finish.
+            `,
+            options: []
+        },
+        {
+            name: `Enate "234" Chardonnay
+            `,
+            price: "Glass $8.99, Bottle $29.99",
+            description: `Complex and intense aromas of green apple, ripe peach, fennel, guava and passion fruit. Round but clean with bracing minerality.
+            `,
+            options: []
+        },
+        {
+            name: `Alto Tierruca Reserve Sauvignon Blanc
+            `,
+            price: "Glass $7.00, Bottle $20.00",
+            description: `This wine expresses rich flavors of lime in balance with fresh acidity in this well-balanced wine. Mineral notes resonate into the long finish.
+            `,
+            options: []
+        }
+    ],
+    "Red Wines": [
+        {
+            name: `Old Soul Cabernet
+            `,
+            price: "Glass $7.99, Bottle $24.99",
+            description: `In this wine you will find purity and elegance, red berry fruit, black cherry, licorice and black pepper all wrapped together with hints of vanilla notes.
+            `,
+            options: []
+        },
+        {
+            name: `Monte Degli Angeli Pinot Noir, Italy
+            `,
+            price: "Glass $7.99, Bottle $24.99",
+            description: `Notes of cherry, strawberry and raspberry. rich, silky and smooth with soft tannins.
+            `,
+            options: []
+        },
+        {
+            name: `Enate Tempranillo, Spain
+            `,
+            price: "Glass $7.99, Bottle $24.99",
+            description: `Blackberries and cherries, strawberry, caramel and licorice. Soft tannins, fresh acidity to give it a rounded creamy finish.
+            `,
+            options: []
+        },
+        {
+            name: `Auga De Piedra Malbec, Argentina
+            `,
+            price: "Glass $7.99, Bottle $24.99",
+            description: `Rich berry fruit smooth spice and earthiness with soft tannins.
+            `,
+            options: []
+        },
+        {
+            name: `Mercedes Egueren Cabernet Sauvignon, Spain
+            `,
+            price: "Glass $7.99, Bottle $24.99",
+            description: `Blackcurrant, licorice, smoke and light herbal flavors, polished texture and a long, fresh finish.
+            `,
+            options: []
+        },
+        {
+            name: `Enate Crianza, Spain
+            `,
+            price: "Glass $7.99, Bottle $24.99",
+            description: `Intense, complex aroma, smoky, spicy notes on a background of ripe and red fruit. Balanced tannin, structure, a long finish including fine toasty notes.
+            `,
+            options: []
+        },
+        {
+            name: `Enate Cabernet, Spain
+            `,
+            price: "Glass $8.99, Bottle $29.99",
+            description: `Complex nose of blackcurrant, blackberry and violet with traces of pepper and vanilla. Flavorful in the mouth, with toasty notes.
+            `,
+            options: []
+        },
+        {
+            name: `White and Red Sangria
+            `,
+            price: "$8.99",
+            description: `A flavorful combination freshly made in house with brandy and fresh fruit, served over ice.
+            `,
+            options: []
+        },
+    ],
+    "Cocktails": [
+        {
+            name: `Mojito
+            `,
+            price: "$8.99",
+            description: `Bacardi Superior Rum combined with sugar cane, lime juice, soda water and mint leaves
+            `,
+            options: []
+        },
+        {
+            name: `Caipirinha
+            `,
+            price: "$8.99",
+            description: `Cachaca (sugar cane liqueur) combined with fresh lime and sugar
+            `,
+            options: []
+        },
+        {
+            name: `Pisco Sour
+            `,
+            price: "$8.99",
+            description: `Pisco (grape brandy) combined with lemon juice and egg white froth
+            `,
+            options: []
+        },
+        {
+            name: `Grand Margarita
+            `,
+            price: "$10.99",
+            description: `Conquistador Tequila combined with triple sec, lime. On the rocks or frozen with or without a salt rim.
+            `,
+            options: ["Top shelf +$3 - ", "On the Rocks - ", "Frozen - ", "With Salt Rim - ", "Without Salt Rim"]
+        },
+        {
+            name: `Margarita Pequeño
+            `,
+            price: "$7.99",
+            description: `A smaller version of our Grand Margarita
+            `,
+            options: ["On the Rocks - ", "Frozen - ", "With Salt Rim - ", "Without Salt Rim"]
+        }
+    ],
+    "Fall Special Drinks": [
+        {
+            name: `Tequila Orange Crush
+            `,
+            price: "$9.99",
+            description: `Casamigos Reposado Tequila combined with orange juice, triple-sec and a splash of lemon-lime soda.
+            `,
+            options: ["Also available with Blanco Tequila"]
+        },
+        {
+            name: `Patron Paloma
+            `,
+            price: "$8.99",
+            description: `Patron Silver Tequila combined with grapefruit juice, lime juice, two lime wedges
+            `,
+            options: []
+        },
+        {
+            name: `Spiced Apple Margarita
+            `,
+            price: "$9.99",
+            description: `Casamigos Reposado Tequila combined with apple cider, fresh lemon juice and agave nectar. topped with a dash of Cinnamon
+            `,
+            options: []
+        },
+        {
+            name: `Mezcal Mule
+            `,
+            price: "$8.99",
+            description: `El Silencio Mezcal combined with ginger beer and lime juice
+            `,
+            options: []
+        },
+        {
+            name: `Anejo Fashion
+            `,
+            price: "$10.99",
+            description: `Casamigos Anejo Tequila combined with simple syrup and Angostura bitters
+            `,
+            options: []
+        },
+    ],
+    "Beers": [
+        {
+            name: `Coors Light
+            `,
+            price: "$4.75",
+            description: `
+            `,
+            options: []
+        },
+        {
+            name: `Corona
+            `,
+            price: "$4.75",
+            description: `
+            `,
+            options: []
+        },
+        {
+            name: `Corona Light
+            `,
+            price: "$4.75",
+            description: `
+            `,
+            options: []
+        },
+        {
+            name: `Dos Equis Lager
+            `,
+            price: "$4.75",
+            description: `
+            `,
+            options: []
+        },
+        {
+            name: `Dos Equis Amber
+            `,
+            price: "$4.75",
+            description: `
+            `,
+            options: []
+        },
+        {
+            name: `Pacifico
+            `,
+            price: "$4.75",
+            description: `
+            `,
+            options: []
+        },
+        {
+            name: `Tecate
+            `,
+            price: "$4.75",
+            description: `
+            `,
+            options: []
+        },
+        {
+            name: `Modelo Especial
+            `,
+            price: "$4.75",
+            description: `
+            `,
+            options: []
+        },
+        {
+            name: `Modelo Negra
+            `,
+            price: "$4.75",
+            description: `
+            `,
+            options: []
+        },
+    ],
+    "Soft Drinks": [
+        {
+            name: `Coke
+            `,
+            price: "$1.95",
+            description: `
+            `,
+            options: []
+        },
+        {
+            name: `Diet Coke
+            `,
+            price: "$1.95",
+            description: `
+            `,
+            options: []
+        },
+        {
+            name: `Sprite
+            `,
+            price: "$1.95",
+            description: `
+            `,
+            options: []
+        },
+        {
+            name: `Pink Lemonade
+            `,
+            price: "$1.95",
+            description: `
+            `,
+            options: []
+        },
     ]
 };
